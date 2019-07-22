b390442f0195ed256cfdb4f27e0ec142c78016a7
Macbook <App@Hamzas-MacBook-Air.local>
2019-07-22 14:08:33 -0400

    Updates

---
+++ precache-manifest.8d192f2b10c0c1a47c8c95a8b4d5356f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "774715978b183f3bf251f68001800b77",
+    "url": "/selector/index.html"
+  },
+  {
+    "revision": "4ccf934d19c3bed15a6e",
+    "url": "/selector/static/css/main.22988873.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/selector/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4ccf934d19c3bed15a6e",
+    "url": "/selector/static/js/main.e228286f.chunk.js"
+  },
+  {
+    "revision": "90afcdfb8efb11a1f025",
+    "url": "/selector/static/js/runtime~main.3573b4bb.js"
+  }
+]);
