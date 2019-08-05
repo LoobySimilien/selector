01198bd6616a15fea2742615a2afe40a387dc442
Student <jsitbon@qcc.cuny.edu>
2019-08-05 13:48:27 -0400

    final edits for a while

--- src/App.js
+++ src/App.js
@@ -6,26 +6,36 @@ class Selector extends Component {
     super ();
     this.state = {
       selectedShape: 'square',
+      count: 0,
     };
-};S
+};
 
 selectShape = (shapeName) => {
   this.setState({
-    selectedShape: shapeName
+    selectedShape: shapeName,
+    count: this.state.count +1
+
   })
 }
+
 
     render() {
       return (
         <div className="container">
           <div className="navbar">
-            <div>Selected:</div>
+            <div className="selection">Selected:</div>
             <div className="shape-list">
                   < Shape  shape="square" selectShape = {this.selectShape} />
                   < Shape  shape="circle" selectShape = {this.selectShape}  />
-                  < Shape  shape="triangle" selectShape = {this.selectShape}  />
+                  < Shape  shape="trapezium" selectShape = {this.selectShape}  />
             </div>
             <div >{ this.state.selectedShape } </div>
+          </div>
+
+          <div className="footerCounter">
+            <div>Number of Clicks:</div>
+            <div className="clickNum" >  {this.state.count} </div>
+
           </div>
         </div>
       );
